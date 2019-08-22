1828495454ba4abd7e715258871bdd02b889431b
Vivian Akinyi <vivianmondi@gmail.com>
2019-08-22 17:08:09 +0300

    Add more users to the user json file

--- users.js
+++ users.js
@@ -4,4 +4,7 @@ module.exports = [
 	{"id":"CR1", "name":"John", "salary":1000, "status":"pending"},
 	{"id":"CR2", "name":"James", "salary":2000, "status":"pending"},
 	{"id":"CR3", "name":"Jane", "salary":1500, "status":"paid"},
+	{"id":"CR4", "name":"Matiangi", "salary":10000, "status":"resolved"},
+	{"id":"CR5", "name":"Orengo", "salary":20500, "status":"pending"},
+	{"id":"CR6", "name":"Karua", "salary":15000, "status":"paid"},
 ];
